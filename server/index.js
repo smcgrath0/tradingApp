0892ca16cd85589cf29eef1383dbcc809a0e30d3
smcgrath0 <smcgrath7777@gmail.com>
2019-12-15 20:20:15 -0800

    index.js clean up

--- server/index.js
+++ server/index.js
@@ -9,17 +9,9 @@ const mongoose = require('mongoose');
 
 const Event = require('./models/event');
 const Stock = require('./models/stock');
-// const companies = require('./companies');
-// const creators = require('./creators');
-// const campaigns = require('./campaigns');
-// const submissions = require('./submissions');
-// const winningAds = require('./winningAds');
-// const user = require('./user');
 
 const server = express();
 let PORT = process.env.PORT;
-
-// connection.connect();
 
 server.use(bodyParser.json());
 
@@ -139,12 +131,7 @@ server.use('/graphql', graphqlHttp({
   },
   graphiql: true
 }));
-// server.use('/api/companies', companies);
-// server.use('/api/creators', creators);
-// server.use('/api/campaigns', campaigns);
-// server.use('/api/submissions', submissions);
-// server.use('/api/winningAds', winningAds);
-// server.use('/api/user', user);
+
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
